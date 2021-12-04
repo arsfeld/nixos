d6a21806d21317d872e78745c1dd19eec134d044
Alexandre Rosenfeld <arsfeld@gmail.com>
2021-12-04 03:09:39 +0000

    Add root user to virgon

--- virgon/configuration.nix
+++ virgon/configuration.nix
@@ -71,6 +71,10 @@
   };
   users.groups.arosenfeld.gid = 1000;
 
+  
+  users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDeQP9ZHuDegrcgBEAuLpCWEK0v8eIBAgaLMSquCP0w" ];
+
+
   services.openssh.enable = true;
 
   networking.firewall.enable = false;
