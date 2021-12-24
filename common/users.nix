d96c03c7ed91d4bac6e7977349273004f835ffe0
Alexandre Rosenfeld <arsfeld@gmail.com>
2021-12-24 16:50:04 +0000

    Add media to groups

--- common/users.nix
+++ common/users.nix
@@ -3,7 +3,7 @@
   users.users.arosenfeld = {
     isNormalUser = true;
     shell = pkgs.zsh;
-    extraGroups = [ "wheel" "docker" "lxd" ];
+    extraGroups = [ "wheel" "docker" "lxd" "media" ];
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDeQP9ZHuDegrcgBEAuLpCWEK0v8eIBAgaLMSquCP0w" ];
     uid = 1000;
   };
