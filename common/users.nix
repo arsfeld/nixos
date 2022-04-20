376ad68c38c2c999d9b04a4a5d6232662f62460c
Alexandre Rosenfeld <arsfeld@gmail.com>
2022-04-20 10:23:36 -0400

    Add default password for arosenfeld

--- common/users.nix
+++ common/users.nix
@@ -6,6 +6,7 @@
     description = "Alexandre Rosenfeld";
     extraGroups = [ "wheel" "docker" "lxd" "media" ];
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDeQP9ZHuDegrcgBEAuLpCWEK0v8eIBAgaLMSquCP0w" ];
+    hashedPassword = "$6$Csmhna5YUVoHnZ/S$lrSk0wko.Z/oL.Omf2jAdLc/mSpZsrw8sOXlknmfdHEjMopP7hESNk9PCArGBnZKm566Fo2QoubQWt0SLjbng.";
     uid = 1000;
   };
   users.groups.arosenfeld.gid = 1000;
