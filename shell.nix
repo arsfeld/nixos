2c6909f9851b2366506fa75a56ab41f8f12eead0
Alexandre Rosenfeld <arsfeld@gmail.com>
2022-04-21 21:23:59 -0400

    Add nix shell

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let 
+  pkgs = import <nixpkgs> {};
+  colmena = import (builtins.fetchTarball 
+    https://github.com/zhaofengli/colmena/tarball/main);
+
+in with pkgs;
+mkShell {
+
+  nativeBuildInputs = [
+    direnv
+    colmena
+  ];
+
+  NIX_ENFORCE_PURITY = true;
+
+  shellHook = ''
+  '';
+}
