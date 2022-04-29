fdb7fc7cf899af33dfd2d03fd5e148747cbd69cc
Alexandre Rosenfeld <arsfeld@gmail.com>
2022-04-28 22:44:51 -0400

    Remove traefik

--- oracle/caddy.nix
+++ oracle/caddy.nix
@@ -7,7 +7,7 @@
   ...
 }:
 with lib; let
-  cloudNode = "localhost";
+  cloudNode = "oracle.arsfeld.net";
   localNode = "striker.arsfeld.net";
   domain = "arsfeld.dev";
   email = "arsfeld@gmail.com";
@@ -24,21 +24,70 @@ in {
     };
   };
 
-  #users.users.caddy.extraGroups = ["acme"];
+  users.users.caddy.extraGroups = ["acme"];
 
   networking.firewall.allowedTCPPorts = [22 80 443];
 
   services.caddy = {
-    enable = false;
+    enable = true;
     email = email;
+    package = pkgs.callPackage ../pkgs/caddy.nix {
+      plugins = [
+        "github.com/greenpau/caddy-security"
+      ];
+      vendorSha256 = "sha256-TAENwTcwppwytl/ti6HGKkh6t9OjgJpUx7NwuGf+PCg=";
+    };
     virtualHosts = {
-      "files.${domain}" = {
-        useACMEHost = domain;
-        extraConfig = "reverse_proxy ${localNode}:8334";
-      };
+      # Cloud
       "vault.${domain}" = {
         useACMEHost = domain;
-        extraConfig = "reverse_proxy ${localNode}:8888";
+        extraConfig = "reverse_proxy ${cloudNode}:8888";
+      };
+      "code.${domain}" = {
+        useACMEHost = domain;
+        extraConfig = "reverse_proxy ${cloudNode}:8443";
+      };
+      "yarr.${domain}" = {
+        useACMEHost = domain;
+        extraConfig = "reverse_proxy ${cloudNode}:7070";
+      };
+
+      # Local
+      "files.${domain}" = {
+        useACMEHost = domain;
+        extraConfig = "reverse_proxy ${cloudNode}:8334";
+      };
+      "nzbhydra2.${domain}" = {
+        useACMEHost = domain;
+        extraConfig = "reverse_proxy ${cloudNode}:5076";
+      };
+      "jackett.${domain}" = {
+        useACMEHost = domain;
+        extraConfig = "reverse_proxy ${localNode}:9117";
+      };
+      "nzbget.${domain}" = {
+        useACMEHost = domain;
+        extraConfig = "reverse_proxy ${localNode}:6789";
+      };
+      "radarr.${domain}" = {
+        useACMEHost = domain;
+        extraConfig = "reverse_proxy ${localNode}:7878";
+      };
+      "sonarr.${domain}" = {
+        useACMEHost = domain;
+        extraConfig = "reverse_proxy ${localNode}:8989";
+      };
+      "sabnzbd.${domain}" = {
+        useACMEHost = domain;
+        extraConfig = "reverse_proxy ${localNode}:8880";
+      };
+      "stash.${domain}" = {
+        useACMEHost = domain;
+        extraConfig = "reverse_proxy ${localNode}:9999";
+      };
+      "syncthing.${domain}" = {
+        useACMEHost = domain;
+        extraConfig = "reverse_proxy ${localNode}:8384";
       };
     };
   };
