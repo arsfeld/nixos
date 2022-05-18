b74dac601a30cf20570c8befca83623f2217440d
Alexandre Rosenfeld <arsfeld@gmail.com>
2022-05-17 20:12:34 -0400

    Fix style issues in r2s

--- machines/r2s/helpers.nix
+++ machines/r2s/helpers.nix
@@ -1,23 +1,21 @@
 {
-  getIPRangePrefix = ipRange: 
+  getIPRangePrefix = ipRange:
     builtins.head (
-      builtins.match 
-        "([0-9][0-9]?[0-9]?\\.[0-9][0-9]?[0-9]?\\.[0-9][0-9]?[0-9]?\\.).*"
-        ipRange
+      builtins.match
+      "([0-9][0-9]?[0-9]?\\.[0-9][0-9]?[0-9]?\\.[0-9][0-9]?[0-9]?\\.).*"
+      ipRange
     );
 
   testIpRange = ipRange:
     builtins.isList (
       builtins.match
-        "([0-9]{1,3}\\.){3}[0-9]{1,3}/24"
-        ipRange
+      "([0-9]{1,3}\\.){3}[0-9]{1,3}/24"
+      ipRange
     );
 
-  commaSeparated = list:
-    let
-      head = builtins.head list;
-      tail = builtins.tail list;
-    in
-      builtins.foldl' (x: y: x + ", " + y) head tail;
-
+  commaSeparated = list: let
+    head = builtins.head list;
+    tail = builtins.tail list;
+  in
+    builtins.foldl' (x: y: x + ", " + y) head tail;
 }
