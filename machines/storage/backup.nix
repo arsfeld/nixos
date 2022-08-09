1750e8220bcdfaaa4478b9fd60e85eb8b77175c6
Alexandre Rosenfeld <arsfeld@gmail.com>
2022-08-09 13:58:39 -0400

    [storage] Remove duplicated rclone

--- machines/storage/backup.nix
+++ machines/storage/backup.nix
@@ -51,24 +51,4 @@ with lib; {
       };
     };
   };
-
-  systemd = {
-    timers.rclone-sync = {
-      wantedBy = ["timers.target"];
-      partOf = ["rclone-sync.service"];
-      timerConfig.OnCalendar = "daily";
-    };
-    services.rclone-sync = let
-      rcloneOptions = "--fast-list --stats-one-line --verbose";
-    in {
-      serviceConfig.Type = "oneshot";
-      serviceConfig.User = "arosenfeld";
-      script = ''
-        ${pkgs.rclone}/bin/rclone sync ${rcloneOptions} dropbox: ~/Dropbox
-        ${pkgs.rclone}/bin/rclone sync ${rcloneOptions} gdrive: ~/Google\ Drive
-        ${pkgs.rclone}/bin/rclone sync ${rcloneOptions} onedrive: ~/One\ Drive
-        ${pkgs.rclone}/bin/rclone sync ${rcloneOptions} box: ~/Box
-      '';
-    };
-  };
 }
