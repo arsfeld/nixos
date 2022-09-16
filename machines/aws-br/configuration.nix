140341f765d2cdb819118e625c7d423a54533247
Alexandre Rosenfeld <arsfeld@gmail.com>
2022-09-15 22:35:56 -0400

    [aws-br] Add new node

---
+++ machines/aws-br/configuration.nix
@@ -0,0 +1,42 @@
+{modulesPath, ...}: {
+  imports = [
+    "${modulesPath}/virtualisation/amazon-image.nix"
+    ../../common/common.nix
+    ../../common/services.nix
+    ../../common/users.nix
+    ../battlestar/rclone-mount.nix
+  ];
+  ec2.hvm = true;
+
+  ec2.efi = true;
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  networking.hostName = "aws-br";
+
+  # services.plex = {
+  #   enable = true;
+  # };
+  # virtualisation.docker.enable = true;
+
+  networking.firewall.allowedTCPPorts = [22 80 443 32400];
+
+  virtualisation.oci-containers.containers."plex" = {
+    environment = {
+      TZ = "America/SaoPaulo";
+      VERSION = "latest";
+    };
+    extraOptions = [ "--network=host" ];
+    image = "linuxserver/plex";
+    volumes = [
+      "/var/lib/plex:/config"
+      "/mnt/media:/mnt/media"
+    ];
+  };
+  services.radarr = {
+    enable = true;
+  };
+  services.sonarr = {
+    enable = true;
+  };
+}
