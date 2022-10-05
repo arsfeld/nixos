5b4b8455a266f7417dae6aed9b4d501cc63e8e87
Alexandre Rosenfeld <arsfeld@gmail.com>
2022-10-05 01:49:45 +0000

    [home] Zellij auto start

--- home/home.nix
+++ home/home.nix
@@ -84,6 +84,8 @@ in {
       if [[ -s /opt/homebrew/bin/brew ]]; then
         eval "$(/opt/homebrew/bin/brew shellenv)"
       fi
+
+      eval "$(zellij setup --generate-auto-start zsh)"
     '';
   };
 
