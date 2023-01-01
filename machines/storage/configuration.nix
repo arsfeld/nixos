ec19213149c562ddf8d2323d8f9d32932245e461
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-01-01 12:35:47 -0500

    [storage] Add nextdns

--- machines/storage/configuration.nix
+++ machines/storage/configuration.nix
@@ -55,13 +55,13 @@ with lib; {
   services.printing.enable = true;
   services.printing.drivers = [pkgs.samsung-unified-linux-driver];
   services.printing.browsing = true;
-  services.printing.allowFrom = [ "all" ]; # this gives access to anyone on the interface you might want to limit it see the official documentation
+  services.printing.allowFrom = ["all"]; # this gives access to anyone on the interface you might want to limit it see the official documentation
   services.printing.defaultShared = true; # If you want
 
   services.avahi.enable = true;
   services.avahi.publish.enable = true;
   services.avahi.publish.userServices = true;
-  
+
   # services.xserver.enable = true;
   # services.xserver.desktopManager.xfce.enable = true;
   # services.xrdp.enable = true;
