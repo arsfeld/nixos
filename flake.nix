4b819b5cc4fbc74cf6ebf492d1e64436736de571
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-01-01 12:34:44 -0500

    [chore] Use devenv

--- flake.nix
+++ flake.nix
@@ -10,9 +10,6 @@
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    colmena.url = "github:zhaofengli/colmena/stable";
-    colmena.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {
@@ -20,139 +17,119 @@
     home-manager,
     nixpkgs,
     utils,
-    colmena,
     nixos-generators,
     ...
-  }: let
-    username = "arosenfeld";
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-  in
-    utils.lib.eachSystem supportedSystems
-    (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [colmena.overlay];
-      };
-    in rec {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          pkgs.home-manager
-          pkgs.colmena
-          pkgs.alejandra
-        ];
-      };
-    })
-    // {
-      colmena = let
-        homeFeatures = [
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.arosenfeld = import ./home/home.nix;
-          }
-        ];
-      in {
-        meta = {
-          nixpkgs = import nixpkgs {
-            system = "x86_64-linux";
-          };
-        };
-
-        micro = {
-          deployment = {
-            targetHost = "micro";
-            tags = ["cloud"];
-          };
-          imports =
-            [
-              ./machines/micro/configuration.nix
-            ]
-            ++ homeFeatures;
-        };
-
-        G14 = {
-          deployment = {
-            targetHost = null;
-            allowLocalDeployment = true;
-            tags = ["local"];
-          };
-          imports =
-            [
-              ./machines/g14/configuration.nix
-            ]
-            ++ homeFeatures;
-        };
-
-        storage = {
-          deployment = {
-            targetHost = "storage";
-            tags = ["local"];
-          };
-          imports =
-            [
-              ./machines/storage/configuration.nix
-            ]
-            ++ homeFeatures;
-        };
-
-        # r2s = {
-        #   nixpkgs.system = "aarch64-linux";
-        #   deployment = {
-        #     targetHost = "r2s";
-        #   };
-        #   imports =
-        #     [
-        #       ./machines/r2s/configuration.nix
-        #     ]
-        #     ++ homeFeatures;
-        # };
-      };
-
-      packages.x86_64-linux = {
-        proxmox = nixos-generators.nixosGenerate {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [
-            # you can include your own nixos configuration here, i.e.
-            # ./configuration.nix
-            ./common/common.nix
-            ./common/users.nix
-          ];
-          format = "proxmox";
+  }: {
+    colmena = let
+      homeFeatures = [
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.arosenfeld = import ./home/home.nix;
+        }
+      ];
+    in {
+      meta = {
+        nixpkgs = import nixpkgs {
+          system = "x86_64-linux";
         };
       };
 
-      homeConfigurations."linux" = let
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.${system};
-          modules = [
-            ./home/home.nix
-            ./home/vscode-ssh-fix.nix
-          ];
+      micro = {
+        deployment = {
+          targetHost = "micro";
+          tags = ["cloud"];
         };
+        imports =
+          [
+            ./machines/micro/configuration.nix
+          ]
+          ++ homeFeatures;
+      };
 
-      homeConfigurations."aarch64" = let
-        system = "aarch64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.${system};
-          modules = [
-            ./home/home.nix
-          ];
+      G14 = {
+        deployment = {
+          targetHost = null;
+          allowLocalDeployment = true;
+          tags = ["local"];
         };
+        imports =
+          [
+            ./machines/g14/configuration.nix
+          ]
+          ++ homeFeatures;
+      };
 
-      homeConfigurations.m1 = let
-        system = "aarch64-darwin";
-      in
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.${system};
-          modules = [
-            ./home/home.nix
-          ];
+      storage = {
+        deployment = {
+          targetHost = "storage";
+          tags = ["local"];
         };
+        imports =
+          [
+            ./machines/storage/configuration.nix
+          ]
+          ++ homeFeatures;
+      };
+
+      # r2s = {
+      #   nixpkgs.system = "aarch64-linux";
+      #   deployment = {
+      #     targetHost = "r2s";
+      #   };
+      #   imports =
+      #     [
+      #       ./machines/r2s/configuration.nix
+      #     ]
+      #     ++ homeFeatures;
+      # };
     };
+
+    packages.x86_64-linux = {
+      proxmox = nixos-generators.nixosGenerate {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          # you can include your own nixos configuration here, i.e.
+          # ./configuration.nix
+          ./common/common.nix
+          ./common/users.nix
+        ];
+        format = "proxmox";
+      };
+    };
+
+    homeConfigurations."linux" = let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          ./home/home.nix
+          ./home/vscode-ssh-fix.nix
+        ];
+      };
+
+    homeConfigurations."aarch64" = let
+      system = "aarch64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          ./home/home.nix
+        ];
+      };
+
+    homeConfigurations.m1 = let
+      system = "aarch64-darwin";
+    in
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          ./home/home.nix
+        ];
+      };
+  };
 }
