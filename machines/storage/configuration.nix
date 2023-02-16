8cbbdb8bbc02822d540c9b4f561d81c2eb95b5ad
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-02-16 22:57:26 +0000

    [storage] Enable hw accel

--- machines/storage/configuration.nix
+++ machines/storage/configuration.nix
@@ -63,6 +63,20 @@ with lib; {
   services.avahi.publish.enable = true;
   services.avahi.publish.userServices = true;
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+    ];
+  };
+
   services.vector = {
     enable = true;
     journaldAccess = true;
