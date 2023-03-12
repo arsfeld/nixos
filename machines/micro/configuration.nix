76cf4c48bb836b6dcb49b2e454f848a44aa0cf17
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-03-12 03:14:50 +0000

    [micro] Add blocky

--- machines/micro/configuration.nix
+++ machines/micro/configuration.nix
@@ -17,6 +17,47 @@
     enable = true;
   };
 
+  services.blocky = {
+    enable = true;
+    settings = {
+      upstream = {
+        default = ["1.1.1.1" "9.9.9.9"];
+      };
+      customDNS = {
+        mapping = {
+          "arsfeld.one" = "192.168.31.15";
+        };
+      };
+      conditional = {
+        rewrite = {
+          lan = "penguin-gecko.ts.net";
+        };
+        mapping = {
+          "ts.net" = "100.100.100.100";
+        };
+      };
+      bootstrapDns = "tcp+udp:1.1.1.1";
+      blocking = {
+        blackLists = {
+          ads = [
+            "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
+            "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+            "http://sysctl.org/cameleon/hosts"
+            "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt"
+            "https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt"
+          ];
+        };
+        clientGroupsBlock = {
+          default = ["ads"];
+        };
+      };
+      prometheus = {
+        enable = true;
+        path = "/metrics";
+      };
+    };
+  };
+
   services.vaultwarden = {
     enable = true;
     config = {
