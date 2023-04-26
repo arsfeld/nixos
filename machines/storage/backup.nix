e7506b638bd3b0d603da774ae924c02a31a44b19
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-04-25 22:43:06 -0400

    [storage] Improve rustic backup

--- machines/storage/backup.nix
+++ machines/storage/backup.nix
@@ -3,84 +3,81 @@
   pkgs,
   lib,
   ...
-}:
-with lib; {
-  services.borgbackup.jobs = {
-    # storage = {
-    #   paths = [ "/var/nas" "/var/lib/plex" "/mnt/data/homes" ];
-    #   environment.BORG_RSH = "ssh -i /root/.ssh/id_ed25519 -o StrictHostKeyChecking=no";
-    #   encryption.passphrase = "ftQoF3LSr9aD7b";
-    #   encryption.mode = "repokey-blake2";
-    #   repo = "storage.arsfeld.ca:repo";
-    #   # repo = "/mnt/backup/repo";
-    #   extraArgs = "--progress";
-    #   compression = "auto,zstd";
-    #   startAt = [ ]; # "daily";
-    # };
+}: let
+  rustic = pkgs.writeShellScriptBin "restic" ''
+    for v in `printenv | grep RESTIC_`
+    do
+        export RUSTIC_''${v#"RESTIC_"}
+    done
+    exec ${pkgs.rustic-rs}/bin/rustic "$@"
+  '';
+  glob = [
+    # very large paths
+    "!/var/lib/docker"
+    "!/var/lib/systemd"
+    "!/var/lib/libvirt"
 
-    data = {
-      paths = [
-        "/mnt/data/homes"
-        "/var/lib"
-        "/var/data"
-        "/root"
-      ];
-      exclude = [
-        # very large paths
-        "/var/lib/docker"
-        "/var/lib/systemd"
-        "/var/lib/libvirt"
+    "!**/.cache"
+    "!**/.nix-profile"
+  ];
+in
+  with lib; {
+    age.secrets."rclone-idrive".file = ../../secrets/rclone-idrive.age;
+    age.secrets."rclone-idrive".mode = "444";
 
-        "'**/.cache'"
-        "'**/.nix-profile'"
-      ];
-      repo = "/mnt/backup/borg";
-      encryption.mode = "none";
-      extraArgs = "--progress";
-      compression = "auto,zstd";
-      startAt = "weekly";
-    };
-  };
+    age.secrets."restic-password".file = ../../secrets/restic-password.age;
+    age.secrets."restic-password".mode = "444";
 
-  age.secrets."rclone-idrive".file = ../../secrets/rclone-idrive.age;
-  age.secrets."rclone-idrive".mode = "444";
+    services.restic.backups = {
+      nas = {
+        paths = ["/var/data"];
+        repository = "/mnt/data/backups/restic";
+        passwordFile = "/etc/secrets/restic";
+        timerConfig = {
+          OnCalendar = "daily";
+        };
+      };
 
-  age.secrets."restic-password".file = ../../secrets/restic-password.age;
-  age.secrets."restic-password".mode = "444";
+      idrive = {
+        package = rustic;
+        repository = "rclone:idrive:arosenfeld";
+        extraBackupArgs = [
+          "--one-file-system"
+          "--glob-file=${pkgs.writeText "glob-file" (concatStringsSep "\n" glob)}"
+          "--progress-interval=1s"
+        ];
+        paths = [
+          "/mnt/data/homes"
+          "/var/lib"
+          "/var/data"
+          "/root"
+        ];
+        rcloneConfigFile = config.age.secrets."rclone-idrive".path;
+        passwordFile = config.age.secrets."restic-password".path;
+        timerConfig = {
+          OnCalendar = "weekly";
+        };
+      };
 
-  services.restic.backups = {
-    nas = {
-      paths = ["/var/data"];
-      repository = "/mnt/data/backups/restic";
-      passwordFile = "/etc/secrets/restic";
-      timerConfig = {
-        OnCalendar = "daily";
+      local = {
+        package = rustic;
+        repository = "/mnt/backup/restic";
+        initialize = true;
+        extraBackupArgs = [
+          "--one-file-system"
+          "--glob-file=${pkgs.writeText "glob-file" (concatStringsSep "\n" glob)}"
+          "--progress-interval=1s"
+        ];
+        paths = [
+          "/mnt/data/homes"
+          "/var/lib"
+          "/var/data"
+          "/root"
+        ];
+        passwordFile = config.age.secrets."restic-password".path;
+        timerConfig = {
+          OnCalendar = "daily";
+        };
       };
     };
-
-    idrive = {
-      package = pkgs.rustic-rs;
-      repository = "rclone:idrive:arosenfeld";
-      paths = [
-        "/mnt/data/homes"
-        "/var/lib"
-        "/var/data"
-        "/root"
-      ];
-      exclude = [
-        # very large paths
-        "/var/lib/docker"
-        "/var/lib/systemd"
-        "/var/lib/libvirt"
-
-        "'**/.cache'"
-        "'**/.nix-profile'"
-      ];
-      rcloneConfigFile = config.age.secrets."rclone-idrive".path;
-      passwordFile = config.age.secrets."restic-password".path;
-      timerConfig = {
-        OnCalendar = "weekly";
-      };
-    };
-  };
-}
+  }
