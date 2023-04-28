2c6ea9d27bc11d08e077844636fdadd4d1e31074
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-04-28 19:32:28 -0400

    [raspi3] Add home-assistant

--- machines/raspi3/configuration.nix
+++ machines/raspi3/configuration.nix
@@ -36,6 +36,45 @@
     enable = true;
   };
 
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers.homeassistant = {
+      volumes = ["/etc/home-assistant:/config"];
+      environment.TZ = "America/Toronto";
+      image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
+      extraOptions = [
+        "--network=host"
+        "--privileged"
+        "--device=/dev/ttyACM0:/dev/ttyACM0" # Example, change this to match your own hardware
+      ];
+    };
+  };
+
+  services.home-assistant = {
+    enable = false;
+    config = {
+      # homeassistant = {
+      #   name = "Home";
+      #   latitude = "!secret latitude";
+      #   longitude = "!secret longitude";
+      #   elevation = "!secret elevation";
+      #   unit_system = "metric";
+      #   time_zone = "America/Toronto";
+      # };
+      #frontend = {
+      #  themes = "!include_dir_merge_named themes";
+      #};
+      # http = {
+      #   server_host = "::1";
+      #   trusted_proxies = ["::1"];
+      #   #use_x_forwarded_for = true;
+      # };
+      #http = {};
+      #feedreader.urls = ["https://nixos.org/blogs.xml"];
+      #lovelace.mode = "storage";
+    };
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
