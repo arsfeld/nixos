c2fa44b46d32411531150fd102db9afbee68412b
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-05-04 23:12:14 -0400

    [pegasus] Add netdata

--- machines/pegasus/configuration.nix
+++ machines/pegasus/configuration.nix
@@ -31,6 +31,10 @@
   # Set your time zone.
   time.timeZone = "America/Toronto";
 
+  services.netdata = {
+    enable = true;
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
