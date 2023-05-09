9650d95e472920cb5dace8c97abd120df957b993
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-05-09 16:55:00 -0400

    [storage] More services refactoring

--- machines/storage/services/backup.nix
+++ machines/storage/services/backup.nix
@@ -12,4 +12,9 @@
     extraFlags = ["--no-auth"];
     dataDir = "/mnt/backup/restic-server";
   };
+
+  services.borgbackup.repos.micro = {
+    authorizedKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2ARxC0ATSCx+aqf66IkUOOwIw6CGwsH47uYXj1+P2U root@micro"];
+    allowSubRepos = true;
+  };
 }
