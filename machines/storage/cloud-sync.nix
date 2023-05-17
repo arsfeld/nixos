1968c4124249aabf0fbf36f92eedea3d7e85d597
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-05-17 11:49:44 -0400

    [storage] Fix sync and add reconnect script

--- machines/storage/cloud-sync.nix
+++ machines/storage/cloud-sync.nix
@@ -5,15 +5,21 @@
   nixpkgs,
   modulesPath,
   ...
-}: let
-  rcloneOptions = "--fast-list --stats-one-line --verbose";
+}:
+with lib; let
   homeDir = "/mnt/data/homes/arosenfeld/Cloud";
+  configFile = "${homeDir}/rclone.conf";
+  rcloneOptions = "--fast-list --stats-one-line --verbose --config ${configFile}";
   providers = {
     dropbox = "Dropbox";
     gdrive = "Google Drive";
     onedrive = "One Drive";
     box = "Box";
   };
+  mkSyncWrapper = provider: folder:
+    pkgs.writeShellScriptBin "sync-${provider}-reconnect" ''
+      ${pkgs.rclone}/bin/rclone config --config ${configFile} reconnect ${provider}:
+    '';
   timers = builtins.foldl' (x: y: x // y) {} (builtins.attrValues (builtins.mapAttrs (provider: folder: {
       "sync-${provider}" = {
         wantedBy = ["timers.target"];
@@ -28,7 +34,7 @@
         serviceConfig.Type = "oneshot";
         serviceConfig.User = "arosenfeld";
         script = ''
-          ${pkgs.rclone}/bin/rclone sync ${rcloneOptions} dropbox: ${homeDir}/${folder}
+          ${pkgs.rclone}/bin/rclone sync ${rcloneOptions} ${provider}: "${homeDir}/${folder}"
         '';
       };
     })
@@ -36,4 +42,5 @@
 in {
   systemd.timers = timers;
   systemd.services = services;
+  environment.systemPackages = with pkgs; (mapAttrsToList mkSyncWrapper providers);
 }
