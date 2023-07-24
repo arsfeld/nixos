d0e40dba312016a965ee2730a0ac2ad36011efe2
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-07-23 22:24:24 -0400

    [router] Add flakes and cockpit

--- machines/router/configuration.nix
+++ machines/router/configuration.nix
@@ -12,14 +12,19 @@ with lib; {
     ./miniupnpd-nftables.nix
     ./network.nix
   ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme"];
   disko.devices = import ./disk-config.nix {
     lib = pkgs.lib;
   };
   boot.loader.systemd-boot.enable = true;
   services.openssh.enable = true;
+  services.cockpit.enable = true;
+  virtualisation.podman.enable = true;
   services.fail2ban.enable = true;
-  services.fail2ban.ignoreIP = [  "192.168.0.0/16"];
+  services.fail2ban.ignoreIP = ["192.168.0.0/16"];
 
   services.netdata.enable = true;
 
