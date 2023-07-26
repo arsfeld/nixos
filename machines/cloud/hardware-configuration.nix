0a3c57a6eca15f8ef510590f464bc166ad0a8507
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-07-25 21:01:10 -0400

    [cloud] Fix boot out of space errors

--- machines/cloud/hardware-configuration.nix
+++ machines/cloud/hardware-configuration.nix
@@ -1,12 +1,18 @@
 { modulesPath, ... }:
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-  boot.loader.grub = {
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-    device = "nodev";
+  boot.loader = {
+    efi = {
+      #canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+    };
+    grub = {
+      efiSupport = true;
+      efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+      device = "nodev";
+    };
   };
-  fileSystems."/boot" = { device = "/dev/disk/by-uuid/6AA5-BC42"; fsType = "vfat"; };
+  fileSystems."/boot/efi" = { device = "/dev/disk/by-uuid/6AA5-BC42"; fsType = "vfat"; };
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
