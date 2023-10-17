069815d9c7a160276b066404ae432dacb735aaca
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-10-16 23:44:22 -0400

    [common] Added ubi key

--- common/users.nix
+++ common/users.nix
@@ -10,7 +10,10 @@
     shell = pkgs.zsh;
     description = "Alexandre Rosenfeld";
     extraGroups = ["wheel" "docker" "lxd" "media" "libvirtd"];
-    openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDeQP9ZHuDegrcgBEAuLpCWEK0v8eIBAgaLMSquCP0w"];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDeQP9ZHuDegrcgBEAuLpCWEK0v8eIBAgaLMSquCP0w"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICobaxx3CHHrOxWLW9Sol7WSaUyLDmG+un2g5K1ongBK alexandre.rosenfeld@ubisoft.com"
+    ];
     hashedPassword = "$6$Csmhna5YUVoHnZ/S$lrSk0wko.Z/oL.Omf2jAdLc/mSpZsrw8sOXlknmfdHEjMopP7hESNk9PCArGBnZKm566Fo2QoubQWt0SLjbng.";
     uid = 1000;
   };
