d71188dfbe037c8fcfc0da727b3b51d9428ab0f8
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-12-03 00:20:33 -0500

    [cloud] DIsable mail server and enable fail2ban

--- machines/cloud/configuration.nix
+++ machines/cloud/configuration.nix
@@ -23,12 +23,22 @@
   # This should be overriden by tailscale at some point
   networking.nameservers = ["1.1.1.1" "9.9.9.9"];
 
+  services.fail2ban = {
+    enable = true;
+    ignoreIP = [
+      "10.0.0.0/8"
+      "172.16.0.0/12"
+      "192.168.0.0/16"
+      "100.64.0.0/10"
+    ];
+  };
+
   security.acme.certs."arsfeld.dev" = {
     extraDomainNames = ["*.arsfeld.dev"];
   };
 
   mailserver = {
-    enable = true;
+    enable = false;
     fqdn = "mail.arsfeld.dev";
     domains = ["arsfeld.dev"];
 
