e93431ff6be55f8e5a78cc11c9d8d637cd7a6231
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-12-10 20:23:16 -0500

    [cloud] Add overseer

--- common/sites/arsfeld.one.nix
+++ common/sites/arsfeld.one.nix
@@ -36,6 +36,7 @@ with lib; let
       "jackett" = "9117";
       "sonarr" = "8989";
       "bazarr" = "6767";
+      "overseer" = "5055";
       "whisparr" = "6969";
       "qbittorrent" = "8080";
       "transmission" = "9091";
