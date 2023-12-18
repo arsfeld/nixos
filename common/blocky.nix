8c22265f7cffa07c26e19a9e6c11eb9b8816f157
Alexandre Rosenfeld <arsfeld@gmail.com>
2023-12-18 11:53:36 -0500

    [storage] Fix blocky

--- common/blocky.nix
+++ common/blocky.nix
@@ -1,10 +1,33 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  blocky_overlay = self: super: {
+    blocky = super.callPackage "${super.path}/pkgs/applications/networking/blocky" {
+      buildGoModule = args:
+        super.buildGoModule (args
+          // {
+            vendorHash = "sha256-9n9IXmzhBB2YRDLiQPUQAdlcHZsn0xK3NZkHPTK5JeA=";
+            version = "git";
+            src = pkgs.fetchFromGitHub {
+              owner = "0xERR0R";
+              repo = "blocky";
+              rev = "abe9e5c46133455eefea620d04c545b91f3f2ca9";
+              sha256 = "qi40sXzZPhDZ90s6HjwLtMz0VfQ9wQkyoNWv3gXDFzw=";
+            };
+          });
+    };
+  };
+in {
+  nixpkgs.overlays = [blocky_overlay];
+
   services.blocky = {
     enable = true;
     settings = {
       upstreams = {
         groups = {
-          default = ["1.1.1.1" "9.9.9.9"];
+          default = ["tcp-tls:security.cloudflare-dns.com"];
         };
       };
       ports = {
@@ -31,7 +54,6 @@
       blocking = {
         blackLists = {
           ads = [
-            "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt"
             "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
           ];
         };
