8053e825c26d63f43478c656b58fb8bd0f1a6fed
Alexandre Rosenfeld <arsfeld@gmail.com>
2024-03-03 00:16:31 -0500

    [storage] Add hetzner backup

---
+++ machines/storage/borg.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  age.secrets."borg-passkey".file = ../../secrets/borg-passkey.age;
+  age.secrets."hetzner".file = ../../secrets/hetzner.age;
+
+  services.borgbackup.jobs.hetzner = {
+    repo = "ssh://u393920@u393920.your-storagebox.de:23/./backups/storage";
+    paths = [
+      "/mnt/data/homes"
+      "/var/lib"
+      "/var/data"
+      "/root"
+    ];
+    exclude = [
+      # very large paths
+      "/var/lib/docker"
+      "/var/lib/systemd"
+      "/var/lib/libvirt"
+
+      "/mnt/data/homes/*/.cache"
+      "/mnt/data/homes/*/.nix-profile"
+    ];
+    extraCreateArgs = "--progress";
+    environment = {
+      BORG_RSH = "ssh -i ${config.age.secrets."hetzner".path}";
+    };
+    encryption = {
+      mode = "repokey-blake2";
+      passCommand = "cat ${config.age.secrets."borg-passkey".path}";
+    };
+    compression = "zstd";
+    startAt = "monthly";
+  };
+}
