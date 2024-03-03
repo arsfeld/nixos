90ef4ff2cb6d2fb444ae97b3de132663c05563ce
Alexandre Rosenfeld <arsfeld@gmail.com>
2024-03-03 16:37:31 -0500

    [home] Remove zellij by default

--- home/home.nix
+++ home/home.nix
@@ -103,9 +103,9 @@ in {
         source $HOME/.cargo/env
       fi
 
-      if [[ -z "$ZELLIJ" && "$TERM_PROGRAM" != "vscode" && -n "$SSH_CLIENT" ]]; then
-        zellij attach -c
-      fi
+      # if [[ -z "$ZELLIJ" && "$TERM_PROGRAM" != "vscode" && -n "$SSH_CLIENT" ]]; then
+      #   zellij attach -c
+      # fi
     '';
     profileExtra = ''
       if [[ -s /etc/set-environment ]]; then
