6afe7746ceee73620dff0eca64b295433cff21de
Alexandre Rosenfeld <arsfeld@gmail.com>
2024-04-21 00:12:15 -0400

    [chore] Cleanup and fix ci

--- flake.nix
+++ flake.nix
@@ -2,9 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/073a26ea454df46ae180207c752ef8c6f6e6ea85";
     nixos-generators.url = "github:nix-community/nixos-generators";
-    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
     disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
     agenix.url = "github:ryantm/agenix";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -13,23 +11,18 @@
     deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haumea.url = "github:nix-community/haumea";
-    haumea.inputs.nixpkgs.follows = "nixpkgs";
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     attic.url = "github:zhaofengli/attic";
-    attic.inputs.nixpkgs.follows = "nixpkgs";
     nixos-flake.url = "github:srid/nixos-flake";
   };
 
   outputs = {self, ...} @ inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} ({moduleWithSystem, ...}: {
-      debug = true;
-
       imports = [
         inputs.devshell.flakeModule
         inputs.treefmt-nix.flakeModule
-        #inputs.colmena-flake.flakeModules.default
         inputs.nixos-flake.flakeModule
       ];
 
@@ -63,6 +56,13 @@
             pkgs.alejandra
             pkgs.deploy-rs
             pkgs.colmena
+          ];
+        };
+
+        legacyPackages.homeConfigurations.arosenfeld = inputs.home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home/home.nix
           ];
         };
       };
@@ -137,72 +137,6 @@
         # };
       };
 
-      # flake.colmena = {
-      #   meta = {
-      #     nixpkgs = import inputs.nixpkgs {
-      #       system = "x86_64-linux";
-      #       config.allowUnfree = true;
-      #     };
-      #     specialArgs.suites = self.nixosSuites;
-      #   };
-
-      #   defaults = moduleWithSystem (
-      #     perSystem @ {
-      #       inputs',
-      #       self',
-      #     }: {lib, ...}: {
-      #       imports =
-      #         [
-      #           inputs.agenix.nixosModules.default
-      #           inputs.home-manager.nixosModules.home-manager
-      #           {
-      #             home-manager.useGlobalPkgs = true;
-      #             home-manager.useUserPackages = true;
-      #             home-manager.users.arosenfeld = import ./home/home.nix;
-      #           }
-      #         ]
-      #         ++ lib.attrValues self.nixosModules;
-      #       _module.args = {
-      #         inputs = perSystem.inputs';
-      #         self = self // perSystem.self'; # to preserve original attributes in self like outPath
-      #       };
-      #       deployment = {
-      #         buildOnTarget = false;
-      #       };
-      #     }
-      #   );
-      # } // builtins.mapAttrs (name: value: { imports = value._module.args.modules; }) self.nixosConfigurations;
-
-      # micro = {...}: {
-      #   imports = [
-      #     ./hosts/micro/configuration.nix
-      #   ];
-      # };
-
-      # cloud = {...}: {
-      #   nixpkgs.system = "aarch64-linux";
-      #   imports = [
-      #     inputs.attic.nixosModules.atticd
-      #     ./hosts/cloud/configuration.nix
-      #   ];
-      # };
-
-      # storage = {...}: {
-      #   imports = [
-      #     ./common/modules/systemd-email-notify.nix
-      #     ./hosts/storage/configuration.nix
-      #   ];
-      # };
-
-      # raider-nixos = {...}: {
-      #   deployment = {
-      #     targetHost = "raider-nixos";
-      #   };
-      #   imports = [
-      #     ./hosts/raider/configuration.nix
-      #   ];
-      # };
-
       # r2s = {...}: {
       #   nixpkgs.system = "aarch64-linux";
       #   imports = [
@@ -211,36 +145,4 @@
       #   ];
       # };
     });
-
-  # homeConfigurations."linux" = let
-  #   system = "x86_64-linux";
-  #   pkgs = nixpkgs.legacyPackages.${system};
-  # in
-  #   home-manager.lib.homeManagerConfiguration {
-  #     pkgs = nixpkgs.legacyPackages.${system};
-  #     modules = [
-  #       ./home/home.nix
-  #     ];
-  #   };
-
-  # homeConfigurations."aarch64" = let
-  #   system = "aarch64-linux";
-  #   pkgs = nixpkgs.legacyPackages.${system};
-  # in
-  #   home-manager.lib.homeManagerConfiguration {
-  #     pkgs = nixpkgs.legacyPackages.${system};
-  #     modules = [
-  #       ./home/home.nix
-  #     ];
-  #   };
-
-  # homeConfigurations.m1 = let
-  #   system = "aarch64-darwin";
-  # in
-  #   home-manager.lib.homeManagerConfiguration {
-  #     pkgs = nixpkgs.legacyPackages.${system};
-  #     modules = [
-  #       ./home/home.nix
-  #     ];
-  #   };
 }
