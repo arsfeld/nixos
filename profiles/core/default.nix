c9f48aa1bdeffcd7a195ec5a5b1e89593f37a242
Alexandre Rosenfeld <arsfeld@gmail.com>
2024-05-10 19:39:43 -0400

    [storage] Remove attic from caches :(

--- profiles/core/default.nix
+++ profiles/core/default.nix
@@ -18,28 +18,28 @@
       substituters = [
         "https://nix-community.cachix.org?priority=41" # this is a useful public cache!
         "https://numtide.cachix.org?priority=42" # this is also a useful public cache!
-        "https://attic.arsfeld.one/system?priority=43"
+        #"https://attic.arsfeld.one/system?priority=43"
         "https://deploy-rs.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-        "system:HOMg/Zztxw6YUqkgtLkotBN0jA0q1xOYiIBtUL6gswY="
+        #"system:HOMg/Zztxw6YUqkgtLkotBN0jA0q1xOYiIBtUL6gswY="
         "deploy-rs.cachix.org-1:xfNobmiwF/vzvK1gpfediPwpdIP0rpDV2rYqx40zdSI="
       ];
 
       netrc-file = config.age.secrets.attic-netrc.path;
     };
 
-    #registry.nixpkgs.flake = nixpkgs;
+    #registry.nixpkgs.flake = inputs.nixpkgs;
 
     # This will add each flake input as a registry
     # To make nix3 commands consistent with your flake
-    #registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
 
     # This will additionally add your inputs to the system's legacy channels
     # Making legacy nix commands consistent as well, awesome!
-    #nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
   };
 
   security.polkit.enable = true;
