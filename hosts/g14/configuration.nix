cfe0a804415ddf0e5d80f6e0182b300d63ebaf5e
Alexandre Rosenfeld <arsfeld@gmail.com>
2024-07-01 23:05:46 -0400

    [chore] Update nixpkgs and rename deprecated options

--- hosts/g14/configuration.nix
+++ hosts/g14/configuration.nix
@@ -51,10 +51,9 @@
 
   #services.switcherooControl.enable = true;
 
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
     extraPackages = with pkgs; [
       vaapiVdpau
       libvdpau-va-gl
