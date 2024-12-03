160600131a6d718f1ca24f9f455f4b7ae10dc019
Alexandre Rosenfeld <arsfeld@gmail.com>
2024-12-03 17:47:02 -0500

    [hpe] Rename pegasus and add config

---
+++ deploy.nix
@@ -0,0 +1,52 @@
+{
+  self,
+  inputs,
+}: {
+  sshUser = "root";
+  autoRollback = false;
+  magicRollback = false;
+  nodes = {
+    storage = {
+      hostname = "storage";
+      profiles.system.path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.storage;
+    };
+    raider = {
+      hostname = "raider-nixos";
+      fastConnection = true;
+      profiles.system.path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.raider;
+    };
+    cloud = {
+      hostname = "cloud";
+      remoteBuild = true;
+      profiles.system.path = inputs.deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.cloud;
+    };
+    cloud-br = {
+      hostname = "cloud-br";
+      profiles.system.path = inputs.deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.cloud-br;
+    };
+    r2s = {
+      hostname = "r2s";
+      fastConnection = true;
+      profiles.system.path = inputs.deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.r2s;
+    };
+    raspi3 = {
+      hostname = "raspi3";
+      fastConnection = true;
+      profiles.system.path = inputs.deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.raspi3;
+    };
+    core = {
+      hostname = "core";
+      profiles.system.path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.core;
+    };
+    g14 = {
+      hostname = "g14";
+      fastConnection = true;
+      profiles.system.path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.g14;
+    };
+    hpe = {
+      hostname = "hpe";
+      fastConnection = true;
+      profiles.system.path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.hpe;
+    };
+  };
+}
---
+++ hosts/hpe/configuration.nix
@@ -0,0 +1,62 @@
+{
+  self,
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  imports =
+    self.nixosSuites.core-vm
+    ++ [
+      ./disko-config.nix
+      ./hardware-configuration.nix
+    ];
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  virtualisation.oci-containers.backend = "podman";
+  virtualisation.docker.enable = false;
+
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    dockerSocket.enable = true;
+  };
+
+  virtualisation.incus.enable = true;
+  networking.nftables.enable = true;
+
+  services.netdata.enable = true;
+
+  boot = {
+    binfmt.emulatedSystems = ["aarch64-linux"];
+    kernelModules = ["kvm-intel"];
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "hpe";
+
+  # networking.interfaces.eno1.ipv4.addresses = [
+  #   {
+  #     address = "192.168.1.182";
+  #     prefixLength = 24;
+  #   }
+  # ];
+
+  networking.bridges = {
+    "br0" = {
+      interfaces = ["eno1"];
+    };
+  };
+  networking.useDHCP = false;
+  networking.interfaces.br0.useDHCP = true;
+
+  #networking.defaultGateway = "192.168.1.1";
+  #networking.nameservers = ["8.8.8.8"];
+
+  services.tailscale.enable = true;
+
+  services.openssh.enable = true;
+}
