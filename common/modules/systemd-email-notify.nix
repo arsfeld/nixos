f21b909f1f2f697ae60c94409a3ee694596d682f
Alexandre Rosenfeld <arsfeld@gmail.com>
2025-01-07 11:56:08 -0500

    [chore] Add rate limit to email failure sending

--- common/modules/systemd-email-notify.nix
+++ common/modules/systemd-email-notify.nix
@@ -10,8 +10,25 @@ with lib; let
   sendmail = pkgs.writeScript "sendmail" ''
     #!/bin/sh
 
+    COOLDOWN_SECONDS=3600  # 1 hour cooldown
+    TIMESTAMP_FILE="/tmp/service_failure_$1.timestamp"
+
+    # Check if the timestamp file exists and if enough time has passed
+    if [ -f "$TIMESTAMP_FILE" ]; then
+      LAST_NOTIFICATION=$(cat "$TIMESTAMP_FILE")
+      CURRENT_TIME=$(date +%s)
+      TIME_DIFF=$((CURRENT_TIME - LAST_NOTIFICATION))
+
+      if [ $TIME_DIFF -lt $COOLDOWN_SECONDS ]; then
+        echo "Rate limit: Not sending email for service $1. Last notification was $TIME_DIFF seconds ago."
+        exit 0
+      fi
+    fi
+
+    # Update the timestamp file
+    date +%s > "$TIMESTAMP_FILE"
     ${sendEmailEvent {
-      event = "Service Failure";
+      event = "Service Failure $1";
       extraContent = ''
         Failed Service: $1
 
