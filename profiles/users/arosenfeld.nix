211babe9e8a3ded4c2c18f996591680c055b7973
Alexandre Rosenfeld <arsfeld@gmail.com>
2025-01-20 15:51:39 -0500

    [chore] Add work key

--- profiles/users/arosenfeld.nix
+++ profiles/users/arosenfeld.nix
@@ -12,6 +12,7 @@
     extraGroups = ["users" "wheel" "docker" "lxd" "media" "libvirtd" "networkmanager" "incus-admin"];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDeQP9ZHuDegrcgBEAuLpCWEK0v8eIBAgaLMSquCP0w"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdOiSH7TgoulL4YasuXaeWC/j3BjGUi9v6168BifciA alexandre.rosenfeld@issuerdirect.com"
     ];
     hashedPassword = "$6$Csmhna5YUVoHnZ/S$lrSk0wko.Z/oL.Omf2jAdLc/mSpZsrw8sOXlknmfdHEjMopP7hESNk9PCArGBnZKm566Fo2QoubQWt0SLjbng.";
     uid = 1000;
