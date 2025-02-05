0dfeea167902d6cbb1306ddbd803970940a290e2
Alexandre Rosenfeld <arsfeld@gmail.com>
2025-02-05 14:34:51 -0500

    [storage] Use rustic to backup

---
+++ hosts/storage/backup/backup-rustic.nix
@@ -0,0 +1,42 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  services.rustic = {
+    enable = true;
+    profiles.nas = {
+      timerConfig = {
+        OnCalendar = "daily";
+        RandomizedDelaySec = "5h";
+      };
+      repository = {
+        repository = "/mnt/data/backups/rustic";
+        password-file = config.age.secrets."restic-password".path;
+      };
+      backup = {
+        init = true;
+        snapshots = [
+          {
+            sources = ["/"];
+            globs = [
+              "!/dev"
+              "!/proc"
+              "!/sys"
+              "!/mnt"
+              "!/media"
+              "!/tmp"
+              "!/var/cache"
+              "!/home/*/.cache"
+              "!/home"
+              "!/run"
+              "!/var/lib/docker"
+              "!/var/lib/lxcfs"
+            ];
+            exclude-if-present = [".nobackup" "CACHEDIR.TAG"];
+          }
+        ];
+      };
+    };
+  };
+}
