0bb2085c5b0708599d755f23191d9db77e862e36
Alexandre Rosenfeld <arsfeld@gmail.com>
2025-02-08 22:29:36 -0500

    [chore] Add docker image update script

--- hosts/storage/services/misc.nix
+++ hosts/storage/services/misc.nix
@@ -18,13 +18,6 @@ in {
   };
 
   virtualisation.oci-containers.containers = {
-    watchtower = {
-      image = "containrrr/watchtower";
-      volumes = [
-        "/var/run/docker.sock:/var/run/docker.sock"
-      ];
-    };
-
     romm = {
       image = "rommapp/romm:latest";
       environment = {
