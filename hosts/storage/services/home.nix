e02679004adea236b2bd7ea0469bf3ef15c89a53
Alexandre Rosenfeld <arsfeld@gmail.com>
2025-02-11 14:43:33 -0500

    [storage] Add finance tracker

--- hosts/storage/services/home.nix
+++ hosts/storage/services/home.nix
@@ -1,7 +1,27 @@
-{config, ...}: let
+{
+  self,
+  config,
+  ...
+}: let
   vars = config.vars;
 in {
+  age.secrets."finance-tracker-env" = {
+    file = "${self}/secrets/finance-tracker-env.age";
+  };
+
   virtualisation.oci-containers.containers = {
+    finance-tracker = {
+      image = "ghcr.io/arsfeld/finance-tracker:latest";
+      volumes = [
+        "${vars.configDir}/finance-tracker:/app/data"
+      ];
+      ports = ["5150:5150"];
+      cmd = ["start" "--server-and-worker"];
+      environmentFiles = [
+        config.age.secrets."finance-tracker-env".path
+      ];
+    };
+
     homeassistant = {
       volumes = ["/var/lib/home-assistant:/config"];
       environment.TZ = "America/Toronto";
