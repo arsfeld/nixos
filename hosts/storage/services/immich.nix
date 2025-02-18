c48a8c6886729a5bc40da345abdeeec8d2911291
Alexandre Rosenfeld <arsfeld@gmail.com>
2025-02-18 17:40:41 -0500

    [bugfix] Fix immich

--- hosts/storage/services/immich.nix
+++ hosts/storage/services/immich.nix
@@ -12,8 +12,6 @@ in {
     mediaLocation = "${vars.dataDir}/files/Immich";
     host = "0.0.0.0";
     port = 15777;
-    user = vars.user;
-    group = vars.group;
     settings = {
       server.externalDomain = "https://immich.${vars.domain}";
       storageTemplate.enabled = true;
