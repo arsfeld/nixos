f6275921479717ec56598ea89ae24225183964ea
Alexandre Rosenfeld <arsfeld@gmail.com>
2025-02-28 23:56:29 -0500

    [chore] Code formatting

--- modules/constellation/docker.nix
+++ modules/constellation/docker.nix
@@ -20,7 +20,7 @@
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
-      
+
       autoPrune.enable = true;
     };
 
