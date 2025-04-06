fd24e592667982d0dc1b1213a9535852c7f104cf
Alexandre Rosenfeld <arsfeld@gmail.com>
2025-04-05 23:22:02 -0400

    ✨ feat: enable docker socket in constellation module

--- modules/constellation/docker.nix
+++ modules/constellation/docker.nix
@@ -21,6 +21,8 @@
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
 
+      dockerSocket.enable = true;
+
       autoPrune.enable = true;
     };
 
