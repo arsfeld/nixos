03953f2c8fb4281d1ed58f8e927791aa93089b06
Alexandre Rosenfeld <arsfeld@gmail.com>
2025-04-10 11:26:00 -0400

    ✨ feat: add podman group to user configuration
    - Update user configuration in users.nix to include "podman" in extraGroups for enhanced container management.

--- modules/constellation/users.nix
+++ modules/constellation/users.nix
@@ -25,7 +25,7 @@
       isNormalUser = true;
       shell = pkgs.fish;
       description = "Alexandre Rosenfeld";
-      extraGroups = ["users" "wheel" "docker" "lxd" "media" "libvirtd" "networkmanager" "incus-admin"];
+      extraGroups = ["users" "wheel" "docker" "lxd" "media" "libvirtd" "networkmanager" "incus-admin" "podman"];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDeQP9ZHuDegrcgBEAuLpCWEK0v8eIBAgaLMSquCP0w"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdOiSH7TgoulL4YasuXaeWC/j3BjGUi9v6168BifciA alexandre.rosenfeld@issuerdirect.com"
