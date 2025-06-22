07cf7bb8d69182b600677a17ddf66212a0388ffe
Alexandre Rosenfeld <arsfeld@gmail.com>
2025-06-21 22:02:28 -0400

    fix: update supabase-manager script to use pkgs for dependencies

--- packages/supabase-manager/default.nix
+++ packages/supabase-manager/default.nix
@@ -1,9 +1,4 @@
-{
-  lib,
-  python3,
-  uv,
-  writeShellScriptBin,
-}:
-writeShellScriptBin "supabase-manager" ''
-  exec ${uv}/bin/uv run --quiet --script ${./supabase-manager.py} "$@"
+{pkgs}:
+pkgs.writeShellScriptBin "supabase-manager" ''
+  exec ${pkgs.uv}/bin/uv run --quiet --script ${./supabase-manager.py} "$@"
 ''
