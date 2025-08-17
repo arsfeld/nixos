5467b4fb4930de0167857366bd900cf51acc690a
Alexandre Rosenfeld <arsfeld@gmail.com>
2025-08-16 21:42:04 -0400

    refactor(hosts): reorganize and clean up host configurations
    - Restructure g14 configuration with constellation module approach
    - Clean up raider host configuration structure
    - Update r2s configuration organization
    - Enable proper constellation modules per host
    - Improve configuration readability and maintainability

--- hosts/g14/configuration.nix
+++ hosts/g14/configuration.nix
@@ -1,28 +1,39 @@
 {
-  self,
   config,
   pkgs,
   lib,
   ...
 }: {
   imports = [
-    ./disko-config.nix
     ./hardware-configuration.nix
+    ./disko-config.nix
   ];
 
+  # Enable constellation modules
+  constellation = {
+    gnome.enable = true;
+    gaming.enable = true;
+    development.enable = true;
+    virtualization.enable = true;
+  };
+
+  # Basic system configuration
+  networking.hostName = "g14";
+
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Use Zen kernel for better desktop performance
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
-  networking.hostName = "G14";
+  # Boot appearance
+  boot.plymouth.enable = true;
+  boot.plymouth.theme = "bgrt";
+  boot.initrd.verbose = false;
+  boot.consoleLogLevel = 0;
 
-  virtualisation.incus = {
-    enable = true;
-    ui.enable = true;
-  };
-  networking.nftables.enable = true;
-
+  # Kernel parameters for performance and power management
   boot.kernelParams = [
     "zswap.enabled=1"
     "mitigations=off"
@@ -31,19 +42,20 @@
     "udev.log_level=0"
   ];
 
-  boot.plymouth.enable = true;
-  boot.plymouth.theme = "bgrt";
-
-  boot.initrd.verbose = false;
-  boot.consoleLogLevel = 0;
-
+  # Remove zfs support
   boot.supportedFilesystems = lib.mkForce ["btrfs" "cifs" "f2fs" "jfs" "ntfs" "reiserfs" "vfat" "xfs" "bcachefs"];
 
-  services.supergfxd.enable = true;
+  # ASUS G14 specific hardware support
+  services.supergfxd.enable = true; # ASUS GPU switching
+  services.asusd = {
+    enable = true;
+    enableUserService = true;
+  };
 
-  services.asusd.enable = true;
-  services.asusd.enableUserService = true;
+  # Networking configuration
+  networking.nftables.enable = true;
 
+  # Network bridge for virtualization
   networking.bridges = {
     "br0" = {
       interfaces = ["enp4s0f4u1"];
@@ -52,14 +64,36 @@
   networking.useDHCP = false;
   networking.interfaces.br0.useDHCP = true;
 
+  # Incus container management (in addition to libvirt from constellation.virtualization)
+  virtualisation.incus = {
+    enable = true;
+    ui.enable = true;
+  };
+
+  # Set your time zone
+  time.timeZone = "America/Toronto";
+
+  # Select internationalisation properties
+  i18n.defaultLocale = "en_CA.UTF-8";
+
   # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "alt-intl";
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "alt-intl";
   };
 
   # Configure console keymap
   console.keyMap = "us";
 
-  system.stateVersion = "23.11"; # Did you read the comment?
+  # Enable the OpenSSH daemon
+  services.openssh.enable = true;
+
+  # Disable firewall for development
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  system.stateVersion = "23.11";
 }
