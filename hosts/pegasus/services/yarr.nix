5de378e832416a1938a48d9d26374e6ac3a7049f
Alexandre Rosenfeld <arsfeld@gmail.com>
2026-06-13 22:18:22 -0400

    feat(pegasus): add yarr rss reader service
    Import yarr from galactica with DB restored from restic backup.
    Served via Cloudflare tunnel at yarr.arsfeld.xyz with bypassAuth
    (no Authelia on pegasus; yarr's built-in login protects it).

---
+++ hosts/pegasus/services/yarr.nix
@@ -0,0 +1,24 @@
+{
+  self,
+  config,
+  lib,
+  ...
+}: let
+  mkService = import "${self}/modules/media/__mkService.nix" {inherit lib;};
+in
+  lib.mkMerge [
+    {sops.secrets."yarr-env" = {};}
+
+    (mkService "yarr" {
+      port = 7070;
+      image = "ghcr.io/arsfeld/yarr:master";
+      bypassAuth = true;
+      container = {
+        exposePort = 7070;
+        configDir = "/data";
+        environmentFiles = [
+          config.sops.secrets."yarr-env".path
+        ];
+      };
+    })
+  ]
