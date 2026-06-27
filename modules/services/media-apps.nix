3848160e36d01822243df7f2ceda7472d3a6d00e
Alexandre Rosenfeld <arsfeld@gmail.com>
2026-06-27 18:56:32 -0400

    refactor(modules): migrate constellation service modules to media.services

--- modules/services/media-apps.nix
+++ modules/services/media-apps.nix
@@ -2,11 +2,9 @@
 {
   config,
   lib,
-  self,
   ...
 }: let
   cfg = config.constellation.mediaApps;
-  mkService = import "${self}/modules/media/__mkService.nix" {inherit lib;};
   vars = config.media.config;
   pia = config.constellation.pia;
 in {
@@ -19,81 +17,87 @@ in {
       sops.secrets.mydia-env.mode = "0444";
     }
 
-    (mkService "ohdio" {
-      port = 4000;
-      image = "ghcr.io/arsfeld/ohdio:latest";
-      container = {
-        environment = {
-          PHX_HOST = "ohdio.arsfeld.one";
-          PORT = "4000";
-          MIX_ENV = "prod";
-          DATABASE_PATH = "/config/db/ohdio_prod.db";
-          STORAGE_PATH = "/config/downloads";
-          MAX_CONCURRENT_DOWNLOADS = "3";
-          CHECK_ORIGIN = "https://ohdio.arsfeld.one,https://ohdio.bat-boa.ts.net";
+    {
+      media.services.ohdio = {
+        port = 4000;
+        image = "ghcr.io/arsfeld/ohdio:latest";
+        container = {
+          environment = {
+            PHX_HOST = "ohdio.arsfeld.one";
+            PORT = "4000";
+            MIX_ENV = "prod";
+            DATABASE_PATH = "/config/db/ohdio_prod.db";
+            STORAGE_PATH = "/config/downloads";
+            MAX_CONCURRENT_DOWNLOADS = "3";
+            CHECK_ORIGIN = "https://ohdio.arsfeld.one,https://ohdio.bat-boa.ts.net";
+          };
+          environmentFiles = [
+            config.sops.secrets.ohdio-env.path
+          ];
         };
-        environmentFiles = [
-          config.sops.secrets.ohdio-env.path
-        ];
+        bypassAuth = true;
       };
-      bypassAuth = true;
-    })
+    }
 
-    (mkService "qui" {
-      port = 7476;
-      image = "ghcr.io/autobrr/qui";
-      container = {
-        environment = {
-          QUI__HOST = "0.0.0.0";
-          QUI__PORT = "7476";
-          QUI__OIDC_ENABLED = "true";
-          QUI__OIDC_ISSUER = "https://auth.arsfeld.one";
-          QUI__OIDC_CLIENT_ID = "qui";
-          QUI__OIDC_REDIRECT_URL = "https://qui.arsfeld.one/api/auth/oidc/callback";
-          QUI__OIDC_DISABLE_BUILT_IN_LOGIN = "false";
+    {
+      media.services.qui = {
+        port = 7476;
+        image = "ghcr.io/autobrr/qui";
+        container = {
+          environment = {
+            QUI__HOST = "0.0.0.0";
+            QUI__PORT = "7476";
+            QUI__OIDC_ENABLED = "true";
+            QUI__OIDC_ISSUER = "https://auth.arsfeld.one";
+            QUI__OIDC_CLIENT_ID = "qui";
+            QUI__OIDC_REDIRECT_URL = "https://qui.arsfeld.one/api/auth/oidc/callback";
+            QUI__OIDC_DISABLE_BUILT_IN_LOGIN = "false";
+          };
+          environmentFiles = [
+            config.sops.secrets.qui-oidc-env.path
+          ];
+          extraOptions = [
+            "--no-healthcheck"
+          ];
         };
-        environmentFiles = [
-          config.sops.secrets.qui-oidc-env.path
-        ];
-        extraOptions = [
-          "--no-healthcheck"
-        ];
+        bypassAuth = true;
       };
-      bypassAuth = true;
-    })
+    }
 
-    (mkService "mydia" {
-      port = 4000;
-      image = "ghcr.io/getmydia/mydia:master";
-      watchImage = true;
-      container = {
-        exposePort = 4000;
-        mediaVolumes = true;
-        network = "host";
-        devices = ["/dev/dri:/dev/dri"];
-        environment = {
-          PHX_HOST = "mydia.arsfeld.one";
-          PORT = "4000";
-          TV_PATH = "/media/Series";
-          MOVIES_PATH = "/media/Movies";
-          OIDC_REDIRECT_URI = "https://mydia.arsfeld.one/auth/oidc/callback";
-          FLARESOLVERR_ENABLED = "true";
-          FLARESOLVERR_URL = "http://localhost:8191";
-          ENABLE_REMOTE_ACCESS = "true";
-          DOWNLOAD_CLIENT_1_NAME = "rqbit";
-          DOWNLOAD_CLIENT_1_TYPE = "rqbit";
-          DOWNLOAD_CLIENT_1_ENABLED = "true";
-          DOWNLOAD_CLIENT_1_PRIORITY = "1";
-          DOWNLOAD_CLIENT_1_HOST = pia.namespaceAddress;
-          DOWNLOAD_CLIENT_1_PORT = "3030";
-          DOWNLOAD_CLIENT_1_USE_SSL = "false";
-          DOWNLOAD_CLIENT_1_DOWNLOAD_DIRECTORY = "${vars.storageDir}/media/Downloads/rqbit";
+    {
+      media.services.mydia = {
+        port = 4000;
+        image = "ghcr.io/getmydia/mydia:master";
+        watchImage = true;
+        container = {
+          exposePort = 4000;
+          mediaVolumes = true;
+          network = "host";
+          devices = ["/dev/dri:/dev/dri"];
+          environment = {
+            PHX_HOST = "mydia.arsfeld.one";
+            PORT = "4000";
+            TV_PATH = "/media/Series";
+            MOVIES_PATH = "/media/Movies";
+            OIDC_REDIRECT_URI = "https://mydia.arsfeld.one/auth/oidc/callback";
+            FLARESOLVERR_ENABLED = "true";
+            FLARESOLVERR_URL = "http://localhost:8191";
+            ENABLE_REMOTE_ACCESS = "true";
+            DOWNLOAD_CLIENT_1_NAME = "rqbit";
+            DOWNLOAD_CLIENT_1_TYPE = "rqbit";
+            DOWNLOAD_CLIENT_1_ENABLED = "true";
+            DOWNLOAD_CLIENT_1_PRIORITY = "1";
+            DOWNLOAD_CLIENT_1_HOST = pia.namespaceAddress;
+            DOWNLOAD_CLIENT_1_PORT = "3030";
+            DOWNLOAD_CLIENT_1_USE_SSL = "false";
+            DOWNLOAD_CLIENT_1_DOWNLOAD_DIRECTORY = "${vars.storageDir}/media/Downloads/rqbit";
+          };
+          environmentFiles = [
+            config.sops.secrets.mydia-env.path
+          ];
         };
-        environmentFiles = [
-          config.sops.secrets.mydia-env.path
-        ];
+        bypassAuth = true;
       };
-      bypassAuth = true;
-    })
+    }
   ]);
 }
--- modules/services/media-automation.nix
+++ modules/services/media-automation.nix
@@ -2,66 +2,64 @@
 {
   config,
   lib,
-  self,
   ...
 }: let
   cfg = config.constellation.mediaAutomation;
-  mkService = import "${self}/modules/media/__mkService.nix" {inherit lib;};
   vars = config.media.config;
 in {
   options.constellation.mediaAutomation.enable = lib.mkEnableOption "media automation services (Radarr, Sonarr, Bazarr, Prowlarr, etc.)";
 
-  config = lib.mkIf cfg.enable (lib.mkMerge [
-    (mkService "radarr" {
+  config = lib.mkIf cfg.enable {
+    media.services.radarr = {
       port = 7878;
       container = {
         mediaVolumes = true;
       };
-    })
+    };
 
-    (mkService "sonarr" {
+    media.services.sonarr = {
       port = 8989;
       container = {
         mediaVolumes = true;
       };
-    })
+    };
 
-    (mkService "bazarr" {
+    media.services.bazarr = {
       port = 6767;
       container = {
         mediaVolumes = true;
       };
       bypassAuth = true;
-    })
+    };
 
-    (mkService "prowlarr" {
+    media.services.prowlarr = {
       port = 9696;
       container = {
         exposePort = 9696;
       };
       bypassAuth = true;
-    })
+    };
 
-    (mkService "jackett" {
+    media.services.jackett = {
       port = 9117;
       container = {};
       bypassAuth = true;
-    })
+    };
 
-    (mkService "autobrr" {
+    media.services.autobrr = {
       port = 7474;
       image = "ghcr.io/autobrr/autobrr:latest";
       container = {};
       bypassAuth = true;
-    })
+    };
 
-    (mkService "overseerr" {
+    media.services.overseerr = {
       port = 5055;
       container = {};
       bypassAuth = true;
-    })
+    };
 
-    (mkService "flaresolverr" {
+    media.services.flaresolverr = {
       port = 8191;
       image = "ghcr.io/flaresolverr/flaresolverr:latest";
       container = {
@@ -69,9 +67,9 @@ in {
         configDir = null;
       };
       bypassAuth = true;
-    })
+    };
 
-    (mkService "pinchflat" {
+    media.services.pinchflat = {
       port = 8945;
       image = "ghcr.io/kieraneglin/pinchflat:latest";
       container = {
@@ -80,6 +78,6 @@ in {
         ];
       };
       bypassAuth = true;
-    })
-  ]);
+    };
+  };
 }
