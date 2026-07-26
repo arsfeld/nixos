e88eba5495cef2766ad91f37f46511a8eb5d013e
Alexandre Rosenfeld <arsfeld@gmail.com>
2026-07-26 17:47:57 -0400

    test(modules): run immich-pixel-sync unit tests in nix flake check

--- flake-modules/checks.nix
+++ flake-modules/checks.nix
@@ -17,6 +17,14 @@
           harmonia-cache-test = inputs.nixpkgs.legacyPackages.${system}.testers.nixosTest (
             import ../tests/harmonia-cache-test.nix {inherit self inputs;}
           );
+          immich-pixel-sync-test =
+            inputs.nixpkgs.legacyPackages.${system}.runCommand "immich-pixel-sync-test" {
+              nativeBuildInputs = [inputs.nixpkgs.legacyPackages.${system}.python3];
+            } ''
+              cp ${../modules/constellation/immich-pixel-sync}/*.py .
+              python3 -m unittest discover -v -s . -p 'test_*.py'
+              touch $out
+            '';
         }
     )
     inputs.deploy-rs.lib;
